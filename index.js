28b577b30f32a97d1909dc1a469f3c2d3714adba
Calvin-Zhou364 <zhoucalvin134@gmail.com>
2026-05-04 04:06:23 -0400

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function toggleMode() {
-  var element = document.body;
-  element.classList.toggle("light-mode");
-  if (element.classList.contains("light-mode")) {
-        console.log("Light mode on!");
-    } else {
-        console.log("Dark mode on!");
-    }
-}
